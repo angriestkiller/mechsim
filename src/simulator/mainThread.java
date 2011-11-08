da957a6a5e4774211757b94b2a7f0df58569a1d0
Sergey <Sergey@notebook>
2011-11-08 23:31:00 +0200

    Main thread added

---
+++ src/simulator/mainThread.java
@@ -0,0 +1,22 @@
+package simulator;
+
+public class mainThread {
+	mechanicalModel model;
+	public mainThread(int columns , int series,int dt){
+		model =new mechanicalModel(columns ,series,dt);
+	}
+	
+	
+	
+	void beginSimulation(){
+		while(true){
+			//loopstop conditions to be added
+			//looppause conditions to be added
+		//	for(int i =1 ; i<columns+)
+			
+			
+		}
+	}
+	
+
+}
